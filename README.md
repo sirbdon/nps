e2ac05402397062b1996eb44fcd81addeb7759c4
Brandon Pizzacalla <bpizzacalla@gmail.com>
2016-07-08 06:46:35 -0700

    minor tweaks and updates

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Readme for NPS
+
+https://mashe.hawksey.info/2014/07/google-sheets-as-a-database-insert-with-apps-script-using-postget-methods-with-ajax-example/
--- nps-website.js
+++ nps-website.js
@@ -9,11 +9,14 @@ function getParameterByName(name, url) {
 }
 
 function uid() {
-  if (devEnv)  { return 'DEV' + Math.random().toString(36).substr(2, 5) }
+  if (devEnv)  { return 'DEV:::' + Math.random().toString(36).substr(2, 5) }
   if (!devEnv) { return Math.random().toString(36).substr(2, 8) }
 }
 
+var noop = function(){}; // do nothing.
+
 function sendForm(callback) {
+  callback = callback || noop;
     $.ajax({
     url: dbase,
     type: "post",
@@ -34,9 +37,16 @@ function sendForm(callback) {
 
 var 
   dbase = 'https://script.google.com/macros/s/AKfycbxPU5ZpiVUloEdVaZD3ww_T9QOlXOzujh87FSz55gGyVoWy9czo/exec',
-  devEnv = (window.location.hostname === 'netpro.dev');
+  devEnv = (window.location.hostname === 'netpro.dev'),
+  page2 = false,
+  page3 = false;
+
+
 
 $(document).ready(function() {
+
+
+  $('#email').focus()
 
   // set uid to enable tracking multiple submits
   var xuid = uid();
@@ -50,14 +60,19 @@ $(document).ready(function() {
   $('#rating').val(rating)
 
   // send initial values right away in case user doesn't want to add additional info
-  sendForm(function(error, result) { console.log(error); console.log(result); });
+  // if (!page2) { sendForm(); }
 
   var thankHeight = $('#thank-you').height();
   var formHeight = $('#form-container').height();
 
+  function redirect() {
+    window.location.replace("https://growthgenius.io/growth-gigs");
+  }
+
   $('#net-promoter').submit(function(e) {
     e.preventDefault();
     $('#submit').prop( "disabled", true );
+    page3 = true;
     sendForm(function(error, result) {
       if (error) { 
         alert("Something broke... we'd appreciate you letting us know!");
@@ -72,10 +87,74 @@ $(document).ready(function() {
         })
 
         $('#thank-you').fadeOut('fast', function() {
-          $('#thanks-placeholder').fadeIn('fast');
+          $('#thanks-placeholder').fadeIn('fast', function() {
+            setTimeout(redirect, 1500)
+          });
         })
       }
     });
+  })
+
+  $('#form-next').click(function(e) {
+    e.preventDefault();
+    // sendForm();
+    $('#form-pg1').fadeOut('fast', function() {
+      $('#form-pg2').fadeIn('fast');
+      page2 = true;
+      $('#comment').focus();
+    })
+  })
+
+  window.addEventListener("beforeunload", function (e) {
+    if (!page3) {
+      sendForm(); // send form if user tries to close window
+
+      var confirmationMessage = "\o/";
+      e.returnValue = confirmationMessage;     // Gecko, Trident, Chrome 34+
+      return confirmationMessage;              // Gecko, WebKit, Chrome <34
+    }
+    
+  });
+
+
+  var map = {13: false, 16: false};
+
+  $(document).keydown(function(e) {
+    if (e.keyCode in map && page2) {
+          map[e.keyCode] = true;
+          if (map[13] && map[16]) {
+              $('#net-promoter').submit();
+          }
+        }
+    }).keyup(function(e) {
+        if (e.keyCode in map) {
+            map[e.keyCode] = false;
+        }
+    });
+
+  function resetBlockHelper() {
+    $('#helpBlock').fadeOut('slow', function() {
+      $('#helpBlockHolder').fadeIn('fast');
+    })
+  }
+
+  $(document).keypress(function (e) {
+    if (e.which == 13 && !page2) {
+      // $('#form-next').click();
+    }
+
+    if (e.which == 13 && page2) {
+      let comVal = $('#comment').val()
+      let retVal = comVal.concat('\r\n')
+      $('#comment').val(retVal)
+
+      $('#helpBlockHolder').fadeOut('fast', function() {
+        $('#helpBlock').fadeIn('fast', function() {
+          setTimeout(resetBlockHelper, 3500);
+        })
+      })
+      return false;   
+    }
   })
 
 
