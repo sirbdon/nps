cdc624b8ba846acd9066e0e2373c25eeb23d69d1
Brandon Pizzacalla <bpizzacalla@gmail.com>
2016-07-08 23:40:15 -0700

    auto submit on mobile if user abandonds screen

--- nps-website.js
+++ nps-website.js
@@ -1,3 +1,16 @@
+//# Set global variables
+var 
+  dbase    = 'https://script.google.com/macros/s/AKfycbxPU5ZpiVUloEdVaZD3ww_T9QOlXOzujh87FSz55gGyVoWy9czo/exec',
+  devEnv   = (window.location.hostname === 'netpro.dev'),
+  page2    = false,
+  page3    = false,
+  isMobile = false,
+  mobileTimeout1,
+  mobileTimeout2,
+  thankHeight,
+  formHeight;
+
+//# Return url parameters by name
 function getParameterByName(name, url) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
@@ -8,96 +21,125 @@ function getParameterByName(name, url) {
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+//# create unique_id as a crude way to track users if data is POSTed multiple times
 function uid() {
   if (devEnv)  { return 'DEV:::' + Math.random().toString(36).substr(2, 5) }
   if (!devEnv) { return Math.random().toString(36).substr(2, 8) }
 }
 
-var noop = function(){}; // do nothing.
+//# Do nothing function for optional callbacks
+var noop = function(){};
 
+//# Send the form with whatever data is in it; Callback optional, but format is: function(error, result) {}
 function sendForm(callback) {
   callback = callback || noop;
-    $.ajax({
+  
+  // POST serialized form data to google sheet url (dbase)
+  $.ajax({
     url: dbase,
     type: "post",
     data: $('#net-promoter').serialize()
   })
   .done(function(response, textStatus, jqXHR) {
-    console.log("initial post done")
-    console.log(response)
+    console.log("Post done:", response)
+
+    // set page3 to prevent fire of 'beforeunload' on redirect
+    page3 = true
+
+    // call submitAnimations if isMobile to avoid creating 
+    // callback function when sendForm called
+    if (isMobile) { submitAnimations(); }
+
+    // callback(error, result)
     callback(null, response);
   })
   .fail(function(response, textStatus, jqXHR){
-    console.log("initial post ERROR")
-    console.log(response)
+    console.log("Post ERROR:", response)
+
+    // callback(error, result)
     callback(response, null);
     throw new Error(textStatus)
   });
 }
 
-var 
-  dbase = 'https://script.google.com/macros/s/AKfycbxPU5ZpiVUloEdVaZD3ww_T9QOlXOzujh87FSz55gGyVoWy9czo/exec',
-  devEnv = (window.location.hostname === 'netpro.dev'),
-  page2 = false,
-  page3 = false;
+//# fadeOut & fadeIn animations after form submit
+function submitAnimations() {
+
+  // set placeholders to original div heights to ensure smooth fades
+  $('#thanks-placeholder').height(thankHeight);
+  $('#success-container').height(formHeight);
+
+  // replace form with thank you message
+  $('#form-container').fadeOut('fast', function() {
+    $('#success-container').fadeIn('fast');
+  })
+
+  // remove green thank-you notification at top
+  $('#thank-you').fadeOut('fast', function() {
+    $('#thanks-placeholder').fadeIn('fast', function() {
+      
+      // redirect to 'redirect' url variable after timeout
+      setTimeout(redirect, 1500)
+    });
+  })
+}
+
+//# Set redirect after thank-you page
+function redirect() {
+  window.location.replace("https://growthgenius.io/growth-gigs");
+}
 
 
+// =========== DOCUMENT READY ===============
 
 $(document).ready(function() {
 
-
+  //# focus on email field when page loads
   $('#email').focus()
 
-  // set uid to enable tracking multiple submits
-  var xuid = uid();
+  //# set uid to enable tracking multiple submits
+  let xuid = uid();
   $('#uid').val(xuid)
 
-  // set date on form
+  //# set date on form hidden field
   $('#date').val( new Date() )
 
-  // set rating based on URL query
-  var rating = getParameterByName('nps')
+  //# set rating based on URL query (?nps=#); included in URL of email signature
+  let rating = getParameterByName('nps')
   $('#rating').val(rating)
 
-  // send initial values right away in case user doesn't want to add additional info
-  // if (!page2) { sendForm(); }
+  //# Check if mobile device by checking property of div set to appear only when 
+  //# screen width is xs
+  if( $('#is-mobile').css('display') !== 'none' ) { isMobile = true }
 
-  var thankHeight = $('#thank-you').height();
-  var formHeight = $('#form-container').height();
+  //# If mobile device, send form after 30s to capture rating data if user abandonds
+  if (isMobile) { mobileTimeout1 = setTimeout(sendForm, 30000) }
 
-  function redirect() {
-    window.location.replace("https://growthgenius.io/growth-gigs");
-  }
+  // Optional: send initial values right away in case user doesn't want 
+  // to add additional info.
+  // Current workaround: user 'beforeunload' event to stall user on page load, giving enough
+  // time for ajax to send data to form.
+  // However, on mobile a user may not explicitly exit the page, so handle it differently
+    // if (!page2) { sendForm(); }
 
-  $('#net-promoter').submit(function(e) {
-    e.preventDefault();
-    $('#submit').prop( "disabled", true );
-    page3 = true;
-    sendForm(function(error, result) {
-      if (error) { 
-        alert("Something broke... we'd appreciate you letting us know!");
-        $('#submit').prop( "disabled", false );
-      }
-      if (result) {
-        $('#thanks-placeholder').height(thankHeight);
-        $('#success-container').height(formHeight);
+  //# get heights of thank-you and form so on fadeOut they can be replaced
+  //# by placeholder divs with the same height; smooth UX
+  thankHeight = $('#thank-you').height();
+  formHeight = $('#form-container').height();
 
-        $('#form-container').fadeOut('fast', function() {
-          $('#success-container').fadeIn('fast');
-        })
-
-        $('#thank-you').fadeOut('fast', function() {
-          $('#thanks-placeholder').fadeIn('fast', function() {
-            setTimeout(redirect, 1500)
-          });
-        })
-      }
-    });
-  })
-
+  //# Next button listener (email field -> comment field transition)
   $('#form-next').click(function(e) {
     e.preventDefault();
-    // sendForm();
+
+    // prevent autosend of form if next button clicked
+    clearTimeout(mobileTimeout1);
+
+    // set new timeout for mobile in case user abandonds at this point    
+    if (isMobile) { mobileTimeout2 = setTimeout(sendForm, 30000) }   
+
+    // sendForm(); <-- Commented out when sendForm was placed on unbeforeload to hopefully reduce number of duplicate entries in db
+
+    // Hide email field, show comment field and focus on it. Thinking more likely to submit email if split up from comment textarea.
     $('#form-pg1').fadeOut('fast', function() {
       $('#form-pg2').fadeIn('fast');
       page2 = true;
@@ -105,10 +147,40 @@ $(document).ready(function() {
     })
   })
 
-  window.addEventListener("beforeunload", function (e) {
-    if (!page3) {
-      sendForm(); // send form if user tries to close window
+  //# Form submit listener
+  $('#net-promoter').submit(function(e) {
+    e.preventDefault();
 
+    // stop autosend of form
+    clearTimeout(mobileTimeout2);
+
+    // disable submit button                
+    $('#submit').prop( "disabled", true );      
+
+    // trim any extra spaces or enters before submit and
+    // set #comment value to trimmed version
+    let comTrim = $('#comment').val().trim();   
+    $('#comment').val( comTrim );               
+
+    // submit form with callback
+    sendForm(function(error, result) {          
+      if (error) { 
+        alert("Something broke... we'd appreciate you letting us know!");
+        // re-enable submit button
+        $('#submit').prop( "disabled", false );  
+      }
+      if (result) {
+        // hide form & show thank-you, then redirect
+        submitAnimations();                     
+      }
+    });
+  })
+
+  //# When user tries to close tab / window, execute function (fails on mobile if user tab-switches)
+  window.addEventListener("beforeunload", function (e) {
+    // don't run on final form page to avoid alert on redirect
+    if (!page3) {                               
+      sendForm();                               
       var confirmationMessage = "\o/";
       e.returnValue = confirmationMessage;     // Gecko, Trident, Chrome 34+
       return confirmationMessage;              // Gecko, WebKit, Chrome <34
@@ -116,9 +188,11 @@ $(document).ready(function() {
     
   });
 
+//## USING SHIFT + ENTER TO SUBMIT FORM to prevent accidental submit in textarea
+  var map = {13: false, 16: false}; // Store key state. Object keys = keyCode (Enter, Shift, respectively) & values represent keydown state
 
-  var map = {13: false, 16: false};
-
+  //# On any keydown event check vs map && page2, set keydown to TRUE
+  //# Submit form if both ENTER and SHIFT are down at the same time
   $(document).keydown(function(e) {
     if (e.keyCode in map && page2) {
           map[e.keyCode] = true;
@@ -126,34 +200,37 @@ $(document).ready(function() {
               $('#net-promoter').submit();
           }
         }
-    }).keyup(function(e) {
+    }).keyup(function(e) {            // set map key state to false on keyup 
         if (e.keyCode in map) {
             map[e.keyCode] = false;
         }
     });
 
+  //# Hide notice that SHIFT + ENTER is required to submit form
   function resetBlockHelper() {
     $('#helpBlock').fadeOut('slow', function() {
       $('#helpBlockHolder').fadeIn('fast');
     })
   }
 
+  //# ENTER key listener
   $(document).keypress(function (e) {
-    if (e.which == 13 && !page2) {
-      // $('#form-next').click();
+    if (e.which == 13 && !page2) {        // !page2 so enter allows user to progress from email input to comment textarea
+      // $('#form-next').click(); <-- 'ENTER' keypress already interpreted as click
     }
 
-    if (e.which == 13 && page2) {
-      let comVal = $('#comment').val()
-      let retVal = comVal.concat('\r\n')
-      $('#comment').val(retVal)
+    if (e.which == 13 && page2) {         // on ENTER keypress
+      let comVal = $('#comment').val()    // grab current comment textfield value
+      let retVal = comVal.concat('\r\n')  // add spoofed ENTER to end (which is otherwise prevented)
+      $('#comment').val(retVal)           // set comment textfield to original value + newline
 
-      $('#helpBlockHolder').fadeOut('fast', function() {
+      // Alert user that SHIFT + ENTER is required to submit form, then reset after timeout
+      $('#helpBlockHolder').fadeOut('fast', function() { 
         $('#helpBlock').fadeIn('fast', function() {
           setTimeout(resetBlockHelper, 3500);
         })
       })
-      return false;   
+      return false;   // prevent default action (basically preventDefault & stopPropogation)
     }
   })
 
